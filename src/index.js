c080e2116cd79c3e24d06c4a358d2822e031bb6a
hbhalodia <hit.bhalodia@rtcamp.com>
2023-03-28 18:13:14 +0530

    Change server to express

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
-import { startServerAndCreateLambdaHandler, handlers } from '@as-integrations/aws-lambda';
+// import { startStandaloneServer } from '@apollo/server/standalone';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import { expressMiddleware } from '@apollo/server/express4';
+import express from 'express';
+import http from 'http';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+// import { startServerAndCreateLambdaHandler, handlers } from '@as-integrations/aws-lambda';
 
 // Import schema.
 import typeDefs from './schema/index.js';
@@ -11,7 +17,6 @@ import WordPressPostAPI from './datasources/index.js';
 // Import resolvers.
 import resolvers from './resolvers/index.js';
 
-const port = Number.parseInt(process.env.PORT) || 4002;
 const login = 'hitbhalodia';
 const password = 'XS3K R6Ot KUL9 nZTa VW8m i0qB';
 
@@ -24,30 +29,70 @@ const dataSources = () => ({
 	WordPressPostAPI: new WordPressPostAPI(options),
 });
 
+// const server = new ApolloServer({
+// 	typeDefs,
+// 	resolvers,
+// });
+
+// // export const graphqlHandler = startServerAndCreateLambdaHandler(
+// // 	server,
+// // 	handlers.createAPIGatewayProxyEventV2RequestHandler(),
+// // 	{
+// // 		context: async () => {
+// // 			return {
+// // 				dataSources: dataSources(),
+// // 			};
+// // 		},
+// // 	},
+// // );
+
+// const { url } = await startStandaloneServer(server, {
+// 	context: async ({ res, req }) => {
+// 		return {
+// 			dataSources: dataSources(),
+// 		};
+// 	},
+// 	listen: { port: port },
+// 	cors: {
+// 		exposedHeaders: ['X-WP-Total', 'X-WP-TotalPages'],
+// 	},
+// });
+// console.log('yese');
+// console.log(`🚀 Apollo Server running at ${url}`);
+
+const app = express();
+
+const httpServer = http.createServer(app);
+
 const server = new ApolloServer({
 	typeDefs,
 	resolvers,
+	plugins: [
+		ApolloServerPluginDrainHttpServer({ httpServer }),
+	],
 });
 
-// export const graphqlHandler = startServerAndCreateLambdaHandler(
-// 	server,
-// 	handlers.createAPIGatewayProxyEventV2RequestHandler(),
-// 	{
-// 		context: async () => {
-// 			return {
-// 				dataSources: dataSources(),
-// 			};
-// 		},
-// 	},
-// );
+await server.start();
 
-const { url } = await startStandaloneServer(server, {
-	context: async () => {
-		return {
-			dataSources: dataSources(),
-		};
-	},
-	listen: { port: port },
-});
+app.use(
+	'/',
+	cors({
+		origin: 'http://localhost:3000',
+		exposedHeaders: '*',
+	}),
+	bodyParser.json(),
+	expressMiddleware(server, {
+		context: async ({ req, res }) => {
+			return {
+				dataSources: dataSources(),
+				res: res,
+				req: req,
+				responseHeaders: {}
+			}
+		},
+	}),
+);
 
-console.log(`🚀 Apollo Server running at ${url}`);
+await new Promise((resolve) => httpServer.listen({ port: 4002 }, resolve));
+
+console.log(`🚀 Server ready at http://localhost:4002/`);
